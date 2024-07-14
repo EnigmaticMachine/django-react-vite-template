8d8b5f4a94e0b3df42f4d0dfd9c5f5b9398516c0
michael h <michaelhenzl.em@gmail.com>
2024-07-14 10:21:15 +0200

    chore: update flake8 configuration and handle flake8 warnings
    - Updated max-line-length to 108 in .flake8
    - Handled flake8 F403 and F401 warnings in settings/__init__.py

--- backend/settings/__init__.py
+++ backend/settings/__init__.py
@@ -3,8 +3,8 @@ import os
 environment = os.getenv("DJANGO_ENV", "dev")
 
 if environment == "prod":
-    from .prod import *
+    from .prod import *  # noqa: F403, F401
 elif environment == "test":
-    from .test import *
+    from .test import *  # noqa: F403, F401
 else:
-    from .dev import *
+    from .dev import *  # noqa: F403, F401

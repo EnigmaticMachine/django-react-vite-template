84746596e038e4336344ab1006a4ef44b37a7902
michael h <michaelhenzl.em@gmail.com>
2024-07-14 10:09:35 +0200

    feat: add security middleware settings to production configuration

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -4,3 +4,14 @@ from .base import *
 DATABASES = {
     "default": env.db(),
 }
+
+# Security settings (only as a fallback)
+# Django’s security middleware as a fallback
+# to ensure your application is secure even if Nginx configuration is not applied.
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = "DENY"
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_SSL_REDIRECT = True  # Redirect all HTTP traffic to HTTPS if not handled by Nginx

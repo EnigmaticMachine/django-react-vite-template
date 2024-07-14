b5ab98f5eb1045238f5b765803757b9dd46b8f51
michael h <michaelhenzl.em@gmail.com>
2024-07-14 08:51:08 +0200

    feat: add Gzip and Brotli compression middleware

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -45,6 +45,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.gzip.GZipMiddleware",
+    "django_brotli.middleware.BrotliMiddleware",
 ]
 
 
